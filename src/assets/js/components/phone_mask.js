7abfe39f1068e3720ca29b9154f7f30692258911
Sergey Andykhin <mrspudi@inbox.ru>
2023-06-17 18:48:13 +0300

    end project

---
+++ src/assets/js/components/phone_mask.js
@@ -0,0 +1,12 @@
+mask('[data-tel-input]');
+
+// Удаляем '+' если больше ничего не введено, чтобы показать placeholder
+const phoneInputs = document.querySelectorAll('[data-tel-input]');
+phoneInputs.forEach((input)=>{
+	input.addEventListener('input', ()=>{
+		if (input.value == '+') input.value = '';
+	})
+	input.addEventListener('blur', ()=>{
+		if (input.value == '+') input.value = '';
+	})
+});
